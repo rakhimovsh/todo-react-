f89fd62f0cac5958637834859c46a3e5824f0f23
rakhimovsh <shukurullo.com@gmail.com>
2022-03-05 09:40:20 +0500

    initial commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <span className="header__to">TO</span>
+      <span className="header__do">DO</span>
+    </header>
+  );
+}
+
+export default Header;
